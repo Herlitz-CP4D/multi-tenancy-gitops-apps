8112d9439e71ed96f868c904775e15cbcae38e9a
MatsHerlitz <96472332+MatsHerlitz@users.noreply.github.com>
2022-10-13 10:03:58 +0200

    Initial commit

---
+++ mq/environments/ci/triggertemplates/mq-qm-stage-triggertemplate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Set variables
+if [[ -z ${GIT_ORG} ]]; then
+  echo "Please provide environment variable GIT_ORG"
+  exit 1
+fi
+
+# Create Kubernetes Secret yaml
+( echo "cat <<EOF" ; cat mq-qm-stage-triggertemplate.yaml_template ; echo EOF ) | \
+GIT_ORG=${GIT_ORG} \
+sh > mq-qm-stage-triggertemplate.yaml
